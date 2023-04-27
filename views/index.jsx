65c5b470ac1feb661f934a53f3bcb9d3d3e11574
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-26 19:41:25 -0700

    adding mongoose

--- views/index.jsx
+++ views/index.jsx
@@ -1,33 +1,30 @@
-const React = require('react')
-const Default = require('./layouts/Default')
+import React from 'react';
+import Default from './layouts/Default';
 
-function Index ({breads})  {
-    return (
-        <Default>
-        <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
-        <ul>
-        { breads.map((bread, index)=> {
-            return (
-                <li key={index}>
-                    <a href={`/breads/${index}`}>
-                        {bread.name}
-                        </a>
-                     </li>
-                     )
-                     })
-                        }
+function Index({breads}) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      {/* This is a JSX comment. */}
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${bread.id}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
 
-
-        </ul>
-
-        <div className="newButton">
-  <a href="/breads/new"><button>Add a new bread</button></a>
-</div>
-
-      </Default>         
-    )
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+    </Default>
+  );
 }
 
-module.exports = Index
+export default Index
+
