9222f21675eae5b8163c4f81df9d250ebde63056
bellacorcillo <isabellacorcillo@gmail.com>
2023-04-29 10:32:27 -0700

    updating edit and adding data seed bonus

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,24 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+breads.get('/data/seed', (req, res) => {
+  const breadData = [
+    { name: 'Baguette', price: 3.99, hasGluten: true },
+    { name: 'Sourdough', price: 5.99, hasGluten: true },
+    { name: 'Rye', price: 4.99, hasGluten: true },
+    { name: 'Gluten-Free White Bread', price: 6.99, hasGluten: false }
+  ];
+
+  Bread.insertMany(breadData)
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send('Internal Server Error')
+    })
+})
+
 // INDEX
 breads.get('/', (req, res) => {
   Bread.find()
@@ -20,11 +38,13 @@ breads.get('/new', (req, res) => {
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
 })
 
 
@@ -59,22 +79,27 @@ breads.post('/', (req, res) => {
 
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
 
+
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
 })
-
 
 
 module.exports = breads
