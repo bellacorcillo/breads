ccc4b3da44491627293fd7d996995ba8d2fa4c57
bellacorcillo <isabellacorcillo@gmail.com>
2023-05-01 19:44:53 -0700

    second model

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,110 +1,104 @@
 const express = require('express')
-const breads = express.Router()
+const bread_router = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker')
 
-breads.get('/data/seed', (req, res) => {
-  const breadData = [
-    { name: 'Baguette', price: 3.99, hasGluten: true },
-    { name: 'Sourdough', price: 5.99, hasGluten: true },
-    { name: 'Rye', price: 4.99, hasGluten: true },
-    { name: 'Gluten-Free White Bread', price: 6.99, hasGluten: false }
-  ];
-
-  Bread.insertMany(breadData)
-    .then(createdBreads => {
-      res.redirect('/breads')
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(500).send('Internal Server Error')
+// Index:
+bread_router.get('/', async (req, res) => {
+    const foundBakers = await Baker.find().lean()
+    const foundBreads = await Bread.find().limit(10).lean()
+    console.log(foundBreads)
+    res.render('index', {
+        breads: foundBreads,
+        bakers: foundBakers,
+        title: 'Index Page'
     })
 })
 
-// INDEX
-breads.get('/', (req, res) => {
-  Bread.find()
-      .then(foundBreads => {
-          res.render('index', {
-              breads: foundBreads,
-              title: 'Index Page'
-          })
-      })
-})
+
+
 
 // NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
+bread_router.get('/new', (req, res) => {
+    Baker.find()
+        .then(foundBakers => {
+            res.render('new', {
+                bakers: foundBakers
+            })
+        })
 })
 
+
 // EDIT
-breads.get('/:id/edit', (req, res) => {
-  Baker.find()
-    .then(foundBakers => {
-        Bread.findById(req.params.id)
-          .then(foundBread => {
-            res.render('edit', {
-                bread: foundBread, 
-                bakers: foundBakers 
-            })
-          })
-    })
+bread_router.get('/:id/edit', (req, res) => {
+    Baker.find()
+        .then(foundBakers => {
+            Bread.findById(req.params.id)
+                .then(foundBread => {
+                    res.render('edit', {
+                        bread: foundBread,
+                        bakers: foundBakers
+                    })
+                })
+        })
 })
+
 
 
 // SHOW
-breads.get('/:id', (req, res) => {
-  Bread.findById(req.params.id)
-      .populate('baker')
-      .then(foundBread => {
-        res.render('show', {
-            bread: foundBread
+bread_router.get('/:id', (req, res) => {
+    Bread.findById(req.params.id)
+        .populate('baker')
+        .then(foundBread => {
+            const bakedBy = foundBread.getBakedBy()
+            console.log(bakedBy)
+            res.render('show', {
+                bread: foundBread
+            })
         })
-      })
-      .catch(err => {
-        res.send('404')
-      })
+        .catch(err => {
+            res.send('404')
+        })
 })
 
 
 // CREATE
-breads.post('/', (req, res) => {
-  if(!req.body.image) {
-      req.body.image = undefined 
-  }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.create(req.body)
-  res.redirect('/breads')
+bread_router.post('/', (req, res) => {
+    if (!req.body.image) {
+        req.body.image = undefined
+    }
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.create(req.body)
+    res.redirect('/breads')
 })
 
-
-
 // DELETE
-breads.delete('/:id', (req, res) => {
-  Bread.findByIdAndDelete(req.params.id) 
-    .then(deletedBread => { 
-      res.status(303).redirect('/breads')
-    })
+bread_router.delete('/:id', (req, res) => {
+    Bread.findByIdAndDelete(req.params.id)
+        .then(deletedBread => {
+            res.status(303).redirect('/breads')
+        })
 })
 
 // UPDATE
-breads.put('/:id', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
-    .then(updatedBread => {
-      console.log(updatedBread) 
-      res.redirect(`/breads/${req.params.id}`) 
-    })
+bread_router.put('/:id', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(updatedBread => {
+            console.log(updatedBread)
+            res.redirect(`/breads/${req.params.id}`)
+        })
 })
 
 
-module.exports = breads
+module.exports = bread_router
 
   
--- views/index.jsx
+++ views/index.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
-import Default from './layouts/Default';
+const React = require('react')
+const Default = require('./layouts/Default')
 
-function Index({breads}) {
-  return (
-    <Default>
-      <h2>Index Page</h2>
-      {/* <p>I have {breads[0].name} bread!</p> */}
-      {/* This is a JSX comment. */}
-      <ul>
-        {breads.map((bread, index) => {
-          return (
-            <li key={index}>
-              <a href={`/breads/${bread.id}`}>{bread.name}</a>
-            </li>
-          );
-        })}
-      </ul>
+function Index({ breads, bakers, title }) {
+    return (
+        <Default>
+            <h2>We got bread!</h2>
+            <h3>Bakers</h3>
+            <ul>
+                {
+                    bakers.map((baker) => {
+                        return (<li key={baker._id}>
+                            <a href={`/bakers/${baker._id}`}>
+                                {baker.name}
+                            </a>
+                        </li>
+                        )
+                    })
+                }
+            </ul>
 
-      <div className="newButton">
-        <a href="/breads/new">
-          <button>Add a new bread</button>
-        </a>
-      </div>
-    </Default>
-  );
+
+            <h3>Breads</h3>
+            <ul>
+                {
+                    breads.map((bread, index) => {
+                        return (<li key={bread._id}>
+                            <a href={`/breads/${bread._id}`}>
+                                {bread.name}
+                            </a>
+                        </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+
+        </Default>
+
+    )
 }
 
-export default Index
+
+module.exports = Index
 
